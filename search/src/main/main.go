de6c8b3c21da01043ded6ec655725c3df9c7e012
Komkrit <komkrit.vi@tnis.com>
2018-07-11 10:53:05 +0700

    modify main of search

--- search/src/main/main.go
+++ search/src/main/main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 )
 
+type listAllResponse struct {
+	Header listAllHeaderResponse `json:"header"`
+	Body   listAllBodyResponse   `json:"body"`
+}
+
+type listAllHeaderResponse struct {
+	Code        int    `json:"code"`
+	Description string `json:"description"`
+}
+
+type listAllBodyResponse struct {
+	Productlist []struct {
+		ProductID    int    `json:"product_id"`
+		ProductName  string `json:"product_name"`
+		ProductPrice string `json:"product_price"`
+		ProductURL   string `json:"product_url"`
+	} `json:"productlist"`
+}
+
 func main() {
-	fmt.Println("Suriya")
+	http.HandleFunc("/products", listAllHandle)
+	http.ListenAndServe(":8080", nil)
+}
+
+func listAllHandle(w http.ResponseWriter, r *http.Request) {
+
 }
